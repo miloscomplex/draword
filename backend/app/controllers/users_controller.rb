f328d221318af3976026f1abfd9a31f19181d3ab
Michael Sutton <hicallmesutton@gmail.com>
2021-04-01 23:47:12 -0500

    Added routes to controller and validation to Models

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -5,4 +5,35 @@ class UsersController < ApplicationController
     render json: users
   end
 
+  def show
+      user = User.find_by_id(params[:id])
+      render json: user
+    end
+
+    def create
+      user = User.find_or_create_by(user_params)
+      user = user.scores.build(score_params)
+      if user.valid? && user.save
+        render json: score
+      else
+        render json: { errors: user.errors }, status: 422
+      end
+    end
+
+  def destroy
+    user = User.find_by_id(params[:id])
+    score.destroy
+    render json: score
+  end
+
+private
+
+  def score_params
+    params.require(:score).permit(:user_id, :points, :time_in_seconds, :guesses)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :initials)
+  end
+
 end
