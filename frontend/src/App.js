07cd073c950ad949b94298ce669ac1c7683c673c
Michael Sutton <hicallmesutton@gmail.com>
2021-04-04 01:42:43 -0500

    Added ChatWindow Component, just bones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CanvasContainer from './containers/CanvasContainer'
+import ChatWindow from './containers/ChatWindow'
 
 function App() {
   return (
     <div className='App'>
       <CanvasContainer />
+      <ChatWindow />
     </div>
   );
 }
---
+++ frontend/src/containers/ChatWindow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ChatWindow extends React.Component {
+
+  render() {
+    return (
+      <div id='chatWindow'>
+        <h1>Chat Window Here</h1>
+      </div>
+    )
+  }
+}
+
+export default ChatWindow
