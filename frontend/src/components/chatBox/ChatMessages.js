5a7732dad7ca8ed66091d43760aab135c85fd194
Michael Sutton <hicallmesutton@gmail.com>
2021-04-05 14:38:35 -0500

    Created ChatMessages Component and created relevant CSS

---
+++ frontend/src/components/chatBox/ChatMessages.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class ChatMessages extends React.Component {
+
+  render() {
+    return (
+      <div className='chat-messages'>
+        <p>Display chat messages here</p>
+        <p>Display chat messages here</p>
+        <p>Display chat messages here</p>
+        <p>Display chat messages here</p>
+        <p>Display chat messages here</p>
+        <p>Display chat messages here</p>
+      </div>
+    )
+  }
+}
+
+export default ChatMessages
--- frontend/src/containers/ChatBox.js
+++ frontend/src/containers/ChatBox.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ChatBoxInput from '../components/chatBox/ChatBoxInput'
+import ChatMessages from '../components/chatBox/ChatMessages'
 
 class ChatBox extends React.Component {
 
   render() {
     return (
       <div id='chatWindow'>
-        <h1>Chat Window Here</h1>
+        <h2>Chat Window</h2>
+        <ChatMessages />
         <ChatBoxInput />
       </div>
     )
