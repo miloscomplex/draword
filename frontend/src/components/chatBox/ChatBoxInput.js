1b1ecd4acb8411ac33b5507f6fe8adf632352ee7
Michael Sutton <hicallmesutton@gmail.com>
2021-04-05 14:52:07 -0500

    Made ChatBoxInput a controlled form

--- frontend/src/components/chatBox/ChatBoxInput.js
+++ frontend/src/components/chatBox/ChatBoxInput.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 class ChatBoxInput extends React.Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
-      <form>
-        <input type='text' />
+      <form onSubmit={ event => this.handleOnSubmit(event) }>
+        <input type='text' value={this.state.text} onChange={event => this.handleOnChange(event) }/>
         <button value='submit'>submit</button>
       </form>
     )
