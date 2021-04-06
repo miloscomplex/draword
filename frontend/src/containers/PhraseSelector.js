1b7b86f7d9581fc63913ee51ebc980e6e93641a2
Michael Sutton <hicallmesutton@gmail.com>
2021-04-06 00:56:17 -0500

    Created phraseSelector Container

---
+++ frontend/src/containers/PhraseSelector.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class PhraseSelector extends React.Component {
+
+  render() {
+    return (
+      <div className='phrase-selector'>
+        Phrase UI goes here
+      </div>
+    )
+  }
+}
+
+export default PhraseSelector
