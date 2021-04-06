073d1343ef0a3404f7dd70ab25956d04d5662615
Michael Sutton <hicallmesutton@gmail.com>
2021-04-06 00:21:12 -0500

    Built Timer Component Functionality

--- frontend/src/components/chatBox/ChatMessages.js
+++ frontend/src/components/chatBox/ChatMessages.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class ChatMessages extends React.Component {
 
   render() {
-    const messages = this.props.messages.map( message => <p>{message}</p>)
-    console.log(this.messages);
+    const messages = this.props.messages.map( (message, index) => <p key={index}>{message}</p> )
+    //console.log(this.messages);
     return (
       <div className='chat-messages'>
         { messages }
--- frontend/src/components/ui/Timer.js
+++ frontend/src/components/ui/Timer.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 
 class Timer extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: 0,
+      isOn: false,
+      start: 0
+    }
+  }
+
+  startTimer = () => {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    })
+    this.timer = setInterval( () => this.setState({
+      time: Math.round( (Date.now() - this.state.start) / 1000 )
+    }), 1000)
+  }
+
+  stopTimer = () => {
+    this.setState({isOn: false})
+    clearInterval(this.timer)
+  }
+
+  resetTimer = () => {
+    this.setState({time: 0, isOn: false})
+  }
+
+  elapsedTime = () => {
+    this.endTime = new Date()
+    let timeDiff = this.endTime - this.startTime
+    // remove miliseconds & round seconds
+    this.stopTime = Math.round(timeDiff / 1000)
+    return this.stopTime
+  }
+
+  componentDidMount() {
+    this.startTimer()
+  }
+
   render() {
     return (
       <div className='timer'>
-        Timer Here
+        Timer Here {this.state.time}
       </div>
     )
   }
