6f1af9959e99e1d228e8d0dc61b287d18b84c8a7
Michael Sutton <hicallmesutton@gmail.com>
2021-04-07 00:29:50 -0500

    moved CSS to folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import Header from './containers/Header.js'
+import './css/Main.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './containers/Header.js'
 import GameContainer from './containers/GameContainer'
-import ChatBox from './containers/ChatBox'
 import Footer from './containers/Footer'
 
 
@@ -12,7 +11,7 @@ function App() {
       <Header />
         <Router>
           <Route path='/' component={GameContainer} />
-          <ChatBox />
+
         </Router>
 
       <Footer />
