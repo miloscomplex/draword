3f87969d4378a7ab064a0781b4a19279f3422316
Michael Sutton <hicallmesutton@gmail.com>
2021-04-07 23:48:55 -0500

    Created Rails backend for actionCable

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,6 +1,10 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    conversation = Conversation.find(params[:conversation])
+    stream_for conversation
+    # stream_from expects a string
+    # stream_for expects an object 
   end
 
   def unsubscribed
---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,23 @@
+class ConversationsController < ApplicationController
+
+  def index
+    conversation = Conversation.all
+    render json: conversation
+  end
+
+  def create
+    conversation = Conversation.new(conversation_params)
+    if conversation.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(ConversConversationSerializer.new(conversation)
+      ).serializerable_hash
+      ActionCable.server.broadcast 'conversations_channel', serialized_data
+      head :ok
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:title)
+  end
+end
