1fb7c4e3c89306c934e72c7d4be5ffbb98580791
Michael Sutton <hicallmesutton@gmail.com>
2021-04-10 17:59:01 -0500

    Added validations to Models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :chats
-  has_many :canvas 
+  has_many :canvas
+
+  validates :title, presence: true
+  validates :title, length: { minimum: 3, maximum: 10 }
 end
