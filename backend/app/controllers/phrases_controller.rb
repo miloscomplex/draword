41e166661c512b583cc2877edd75b34109576cd9
Michael Sutton <hicallmesutton@gmail.com>
2021-04-11 22:54:11 -0500

    created random-phrases route and fixed serializer

--- backend/app/controllers/phrases_controller.rb
+++ backend/app/controllers/phrases_controller.rb
@@ -1,2 +1,17 @@
 class PhrasesController < ApplicationController
+
+  def index
+    phrases = Phrase.all
+    render json: phrases
+  end
+
+  def show
+    phrase = Phrase.find_by(id: params[:id])
+    render json: phrase
+  end
+
+  def show_random
+    random = Phrase.order(Arel.sql('RANDOM()')).first(3)
+    render json: random
+  end
 end
