4e392b96d39cf631c4591fbbc867d253907586ee
Michael Sutton <hicallmesutton@gmail.com>
2021-04-19 16:19:11 -0500

    State does not like child objects it wants primitive data

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -2,5 +2,6 @@ class RoomSerializer < ActiveModel::Serializer
   attributes :id, :title, :selected_phrase_id, :has_drawer
   has_many :chats
   has_many :canvas
-  has_one :phrase
+  # has_one :phrase
+  ### React doesn't like it being a child object
 end
