b68dddf0ac9f2b742e6e5ceaa4ad4934123aed96
Michael Sutton <hicallmesutton@gmail.com>
2021-05-12 01:20:15 -0500

    Refactored Game Controller to remove player on cable unsubscribe

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -46,3 +46,33 @@ class RoomsController < ApplicationController
   end
 
 end
+
+#  REF FOR GAME_PLAY CONTROLLER 
+# def show
+#   game_play = GamePlay.find_by(room_id: params[:room_id])
+#   render json: game_play
+# end
+#
+# def create
+#   # just broadcast don't write to the server
+#   game_play = GamePlay.create(room_id: game_play_params[:room_id]) do |room|
+#     room.action = game_play_params[:action]
+#   end
+#
+#   if game_play.save
+#     serialized_data = game_play_params
+#     ActionCable.server.broadcast "game_plays_channel_#{game_play_params[:room_id]}", game_play_params
+#       # stream_for and broadcast_to to be useful for transmitting data along non-universal channels, such as for members of a particular conversation or specific users
+#       head :ok
+#   end
+# end
+#
+# def receive(data)
+#   ActionCable.server.broadcast "game_plays_channel_#{game_play_params[:room_id]}", "hee hawww"
+# end
+#
+# private
+#
+# def game_play_params
+#   params.require(:game_play).permit(:action, :room_id)
+# end
