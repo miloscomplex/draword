1eca1ef595c704ce7fd8a21f32ec0dc4be804d14
Michael Sutton <hicallmesutton@gmail.com>
2021-05-13 23:18:27 -0500

    Add various ui tweaks to display information.

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -7,4 +7,9 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+  #
+  # def timer
+  #   broadcast_to "room_channel_#{params[:room_id]}"
+  # end
+
 end
