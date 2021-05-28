f54de378be0e809d20fec7d2f4c03621fade0085
Michael Sutton <hicallmesutton@gmail.com>
2021-05-28 00:57:05 -0500

    Fixed migration files, postgresql doesn't like foreign keys added b4 the table is made

--- db/migrate/20210408161542_create_rooms.rb
+++ db/migrate/20210408161542_create_rooms.rb
@@ -3,7 +3,7 @@ class CreateRooms < ActiveRecord::Migration[6.0]
     create_table :rooms do |t|
       t.string :title
       t.string :status
-      t.references :drawer, null: true, foreign_key: { to_table: :users }
+      # t.references :drawer, null: true, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
