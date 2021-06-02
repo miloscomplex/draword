8a05419deed763495ce1444aea9391facb1b53c1
Michael Sutton <hicallmesutton@gmail.com>
2021-06-02 00:56:09 -0500

    adding post fetch for score

--- frontend/src/components/gamePlay/EndOfGame.js
+++ frontend/src/components/gamePlay/EndOfGame.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { editSelectedRoom } from '../../redux/actions'
 import { connect } from 'react-redux'
+import { API_ROOT, PARSE_JSON, HEADERS } from '../../constants'
 
 class EndOfGame extends React.Component {
 
@@ -19,16 +20,29 @@ class EndOfGame extends React.Component {
     this.props.editSelectedRoom({room_id: this.props.selectedRoom.id, status: preplay, selected_phrase_id: null, drawer_id: null })
   }
 
+  handleSubmit = (scoreObj) => {
+    fetch(`${API_ROOT}/scores`, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify(scoreObj),
+    })
+  }
+
   isDrawer = () => {
     const { selectedRoom, user, timer } = this.props
 
     return (
       selectedRoom.drawer_id === user.id &&
       (
-        <div className='phraseReminder'>
-          <h4>Hey there drawer do you want to record your winning?</h4>
-          <p>It looks like it only took you guesser's <strong>{ timer.time }</strong> seconds to guess the phrase <strong>{ selectedRoom.phrase.phrase }</strong></p>
+        <div className='alt-background'>
+          <h4>
+            Hey there drawer do you want to record your winning?
+          </h4>
+          <p>
+            It looks like it only took your guesser's <strong>{ timer.time }</strong> seconds to guess the phrase <strong>{ selectedRoom.phrase.phrase }</strong>
+          </p>
           <button>submit your time</button>
+          <button onClick={ () => this.handleClick() } >maybe next time</button>
         </div>
       )
     )
@@ -43,7 +57,7 @@ class EndOfGame extends React.Component {
           Wow you won ! { `The correct answer was ${selectedRoom.phrase.phrase}`}
         </h2>
         <p>
-          { `Good Job room: ${selectedRoom.title}! This is the end of Game` }
+          { `Good Job room: ${selectedRoom.title}! This is the end of Game. You will be redirected automatically once the drawer starts over or submits your score.` }
         </p>
         <button onClick={ () => this.handleClick() }>start over
         </button>
